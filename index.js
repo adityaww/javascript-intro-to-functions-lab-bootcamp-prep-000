039cd7211f51a222fe6603a7c59dbde9082d1482
Aditya Waruwu <aditya.william96@gmail.com>
2019-01-24 03:28:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return 'hello'.toUpperCase()
+}
+
+function whisper(string) {
+  return 'HELLO'.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  console.log('HELLO'.toLowerCase)
+  return "I can\'t hear you!"
+}
